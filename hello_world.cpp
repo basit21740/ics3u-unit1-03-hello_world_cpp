933d4253567866788f66c5af25732663458d58c6
abdul <abdul.butt@mths.ca>
2021-11-05 17:29:41 +0000

    added comments and functions

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Abdul Basit All rights reserved
+//
+// Created by: Abdul Basit
+// Created on: Nov 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!";
+}
